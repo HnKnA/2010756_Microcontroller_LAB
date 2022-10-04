8f607e7aa89aa2944d781bb6ce48f7a4862e39fb
AT <nguyentronganhtuan2102@gmail.com>
2022-10-04 10:11:11 +0700

    Update Lab 2 Ex 1

---
+++ Lab2/Ex1/STM/TWO_7LED/Core/Inc/software_timer.h
@@ -0,0 +1,12 @@
+
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+extern int timer1_flag;
+
+void setTimer(int duration);
+void timerRun();
+
+
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
